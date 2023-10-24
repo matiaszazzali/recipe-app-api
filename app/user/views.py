04006dbff42e724874157c386752ca3f26be8f64
mati <matizazzali@gmail.com>
2023-10-24 10:36:17 -0300

    add auth token tests, serializers, views and urls

--- app/user/views.py
+++ app/user/views.py
@@ -2,11 +2,22 @@
 Views for the user API.
 """
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+)
 
 
 # CreateAPIView handles a http post request for creating objects
 class CreateUserView(generics.CreateAPIView):
     """Create a ne user in the system."""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
